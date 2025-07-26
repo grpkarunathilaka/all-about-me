702a00e0a34954d2b2b7d12a1d241ed91b636696
grpkarunathilaka@gmail.com <grpkarunathilaka@gmail.com>
2025-07-26 21:28:50 +1000

    Update angular to 20

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +13,8 @@ import { DataService } from '../../services/data.service';
 })
 export class ProjectsComponent implements OnInit {
   projectsData: any[] = [];
+  transform = 0;
+  @ViewChild('carouselInner') carouselInner: ElementRef | undefined;
 
   constructor(private dataService: DataService) { }
 
@@ -20,5 +22,26 @@ export class ProjectsComponent implements OnInit {
     this.dataService.getData().subscribe(data => {
       this.projectsData = data.projects;
     });
+  }
+
+  prev() {
+    if (this.carouselInner) {
+      const itemWidth = this.carouselInner.nativeElement.children[0].clientWidth;
+      this.transform += itemWidth * 2;
+      if (this.transform > 0) {
+        this.transform = 0;
+      }
+    }
+  }
+
+  next() {
+    if (this.carouselInner) {
+      const itemWidth = this.carouselInner.nativeElement.children[0].clientWidth;
+      const totalWidth = this.carouselInner.nativeElement.scrollWidth;
+      const visibleWidth = this.carouselInner.nativeElement.parentElement.clientWidth;
+      if (Math.abs(this.transform) < totalWidth - visibleWidth) {
+        this.transform -= itemWidth * 2;
+      }
+    }
   }
 }
