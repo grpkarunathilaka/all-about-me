b77cfaebea8a468e0e42719c8a39470a5a3da291
grpkarunathilaka@gmail.com <grpkarunathilaka@gmail.com>
2025-07-26 20:15:00 +1000

    Update the design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, ProfileComponent, ProjectsComponent],
+  imports: [CommonModule, ProfileComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-
-export const routes: Routes = [
-  { path: '', component: ProfileComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: '**', redirectTo: '' } // Redirect to home if no matching route
-];
